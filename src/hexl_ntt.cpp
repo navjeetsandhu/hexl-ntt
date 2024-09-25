f965a4e0a523be9d72999ca482387931bf3ee896
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-09-25 06:01:36 -0400

    changes

--- src/hexl_ntt.cpp
+++ src/hexl_ntt.cpp
@@ -20,14 +20,16 @@ namespace hexl_ntt {
         intel::hexl::EltwiseMultMod(result, p1, p2, degree, modulus,1);
     }
 
-    void test_hexl_ntt_nwc(uint64_t *result, const uint64_t *p1, const uint64_t *p2, uint64_t degree, uint64_t modulus) {
+    void test_hexl_ntt_nwc(uint64_t *result, const uint64_t *p1, const uint64_t *p2,
+                           uint64_t degree, uint64_t modulus) {
         auto ntt = hexl_ntt::create_ntt(degree, modulus);
         std::vector<uint64_t> output(degree);
         std::vector<uint64_t> p1_output(degree);
         std::vector<uint64_t> p2_output(degree);
         hexl_ntt::compute_forward(p1_output.data(), p1, ntt);
         hexl_ntt::compute_forward(p2_output.data(), p2, ntt);
-        hexl_ntt::eltwise_mult_mod(output.data(), p1_output.data(), p2_output.data(), degree, modulus);
+        hexl_ntt::eltwise_mult_mod(output.data(), p1_output.data(),
+                                   p2_output.data(), degree, modulus);
         hexl_ntt::compute_inverse(result, output.data(), ntt);
     }
 
